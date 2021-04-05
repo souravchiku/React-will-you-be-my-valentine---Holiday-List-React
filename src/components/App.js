6b33e7b2b4f2ab270e63320a1e7d0c26ebb3a5f3
sourav <souravchiku50@gmail.com>
2021-04-05 23:21:32 +0530

    commit 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,50 +1,31 @@
+import React, { useState } from 'react';
 
-import React, { Component, useState } from "react";
-import '../styles/App.css';
+const App = () => {
+  const [citylist, setcitylist] = useState([
 
-class App extends Component {
-  constructor(props) {
-    super(props)
+    { name: "Goa", country: "India" },
+    { name: "Amsterdam", country: "Netherlands" },
+    { name: "New York", country: "USA" },
+    { name: "Darjeeling", country: "India" },
+    { name: "Tokyo", country: "Japan" },
+    { name: "Lonavala", country: "India" }
 
-    this.cityList = [{ name: 'Goa', country: 'India' },
-    { name: 'Amsterdam', country: 'Netherlands' },
-    { name: 'New York', country: 'USA' },
-    { name: 'Darjeeling', country: 'India' },
-    { name: 'Tokyo', country: 'Japan' },
-    { name: 'Lonavala', country: 'India' },
-    { name: 'Brandenburg Gate', country: 'Germany' },
-    { name: 'Reichstag Building', country: 'Germany' },
-    { name: 'Museum Island', country: 'Germany' },
-    { name: 'Munnar', country: 'India' },
-    { name: 'Leh Ladakh', country: 'India' },
-    { name: 'Goa', country: 'India' },
-    { name: 'Agra', country: 'India' },
-    { name: 'Dalhousie', country: 'India' },
-    { name: 'Coorg', country: 'India' },
-    { name: 'Rome', country: 'Italy' },
-    { name: 'Milan', country: 'Italy' },
-    { name: 'Venice', country: 'Italy' },
-    { name: 'Varanasai', country: 'India' },
-    { name: 'Jaipur', country: 'India' },
-    { name: 'The Hofburg', country: 'Austria' },
-    { name: 'Belvedere Palace', country: 'Austria' },
-    { name: 'St. Stephen Cathedral', country: 'Austria' },
-    { name: 'Kahna National Park', country: 'India' },
-    { name: 'Amritsar', country: 'India' },
-    { name: 'Mussoorie', country: 'India' },
-    { name: 'Mount Abu', country: 'India' },
-    { name: 'Tirupati', country: 'India' },
-    ]
+  ])
+
+  const List=()=>{
+    // let count=0;
+    const city= citylist.map((item,key)=>{
+      if(item.country ==='India')
+      return<li key={`location${key+1}`}>{item.name}</li>
+      console.log(item)
+    })
+    return <o>{city}</o>
   }
-
-  render() {
-    return (
-      <div id="main">
-        {/* Do not remove the main div */}
-      </div>
-    )
-  }
-}
-
+  return (
+    <div>
+      {List() }
+    </div>
+  );
+};
 
 export default App;
